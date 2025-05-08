5f053edde2277b71d37e008fa0bccaf35cf99375
Artur-py345 <artur.chaves@ufrpe.br>
2025-05-08 19:50:11 -0300

    Configuração da tela de recuperação de senha
    adicionado a estrutura e configurações da tela de recuperação de senhas

--- lib/features/auth/screens/recoverypass/recoverypass_screen.dart
+++ lib/features/auth/screens/recoverypass/recoverypass_screen.dart
@@ -5,9 +5,61 @@ class RecoverypassScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Recuperação de Senha')),
-      body: const Center(child: Text('Bem-vindo!')),
+      appBar: AppBar(
+        title: const Text('Recuperação de Senha'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: SizedBox.expand(
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Image.asset(
+                'assets/signup-background.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Digite seu e-mail para recuperação de senha:',
+                      style: TextStyle(fontSize: 26, color: Colors.black),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                        prefixIcon: Icon(Icons.email),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Confirmar'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
