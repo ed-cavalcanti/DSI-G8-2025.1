c18c017c9f2969c5e3a1a7bcd00095f91cfa7417
ed-cavalcanti <edney2809@gmail.com>
2025-05-16 13:10:37 -0300

    [feat] integração com Firebase para autenticação de usuários

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
+import 'package:diainfo/features/auth/tree.dart';
 import 'package:diainfo/features/dashboard/screens/dashboard/dashboard_screen.dart';
 import 'package:diainfo/features/auth/screens/login/login_screen.dart';
 import 'package:diainfo/features/auth/screens/recoverypass/recoverypass_screen.dart';
 import 'package:diainfo/features/auth/screens/signup/signup_screen.dart';
 import 'package:diainfo/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
   );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -22,9 +25,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Diainfo',
       theme: AppTheme.lightTheme,
-      home: SignupScreen(),
-      initialRoute: '/login',
+      home: Tree(),
       routes: {
+        '/tree': (context) => Tree(),
         '/login': (context) => LoginScreen(),
         '/signup': (context) => SignupScreen(),
         '/dashboard': (context) => DashboardScreen(),
